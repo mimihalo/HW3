96d9f17a1fdd0173e0da2fb728e4c306b9daf2c7
mimihalo <mimihalo@miko.tw>
2015-10-08 00:41:00 +0800

    Fix coding style

--- q2/main.c
+++ q2/main.c
@@ -3,17 +3,17 @@
 
 int main()
 {
-	char str[128];
-	char c;
-	char res;
-	printf("str: ");
-	gets(str);
-	printf("c: ");
-	scanf("%c",&c);
-	fflush(stdin);
-	
-	res=smallest_character(str, c);
-	printf("%c\n",res);
-	
-	return 0;
+    char str[128];
+    char c;
+    char res;
+    printf("str: ");
+    gets(str);
+    printf("c: ");
+    scanf("%c",&c);
+    fflush(stdin);
+
+    res=smallest_character(str, c);
+    printf("%c\n",res);
+
+    return 0;
 }
--- q2/q2_rec.c
+++ q2/q2_rec.c
@@ -2,20 +2,18 @@
 
 char smallest_character(char str[], char c)
 {
-	char tmp;
-	if(str[0]!='\0')
-	{
-		if(str[0]>c)
-			return str[0];
-		else
-		{
-			tmp=smallest_character(str+1,c);
-			if(tmp<=c)
-				return str[0];
-			else
-				return tmp;
-		}
-	}
-	return 0;
+    char tmp;
+    if(str[0]!='\0') {
+        if(str[0]>c)
+            return str[0];
+        else {
+            tmp=smallest_character(str+1,c);
+            if(tmp<=c)
+                return str[0];
+            else
+                return tmp;
+        }
+    }
+    return 0;
 }
 
